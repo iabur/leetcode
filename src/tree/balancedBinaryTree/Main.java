b5a1367306c1f83eb66f7dc37809fd2864ff634d
iabur <iaburrahman@brainstation-23.com>
2025-03-13 13:32:56 +0600

    balanced binary tree

---
+++ src/tree/balancedBinaryTree/Main.java
@@ -0,0 +1,16 @@
+package tree.balancedBinaryTree;
+
+public class Main {
+    public static void main(String[] args) {
+// Create a balanced binary tree
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        Solution solution = new Solution();
+        boolean isBalanced = solution.isBalanced(root);
+        System.out.println("Is the binary tree balanced? " + isBalanced);
+    }
+}
