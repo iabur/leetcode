6a0f3df33600d13fe1492bea6fa97a2b10755301
iabur <iaburrahman@brainstation-23.com>
2025-03-14 13:12:31 +0600

    same tree check

--- src/tree/sameTreeCheck/Solution.java
+++ src/tree/sameTreeCheck/Solution.java
@@ -2,18 +2,15 @@ package tree.sameTreeCheck;
 
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
+
         if (p == null && q == null) {
             return true;
         }
-        if (p == null || q == null) {
-            return false;
-        }
-        if (p.val != q.val) {
-            return false;
-        }
-        boolean leftSameTree = isSameTree(p.left, q.left);
-        boolean rightSameTree = isSameTree(p.right, q.right);
 
-        return leftSameTree && rightSameTree;
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
