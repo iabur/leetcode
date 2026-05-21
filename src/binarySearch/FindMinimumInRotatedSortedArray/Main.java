7300a67430b46cb8865d0b60f96f772bfe1ce160
iabur <rahman.iabur@technonext.com>
2026-05-21 18:49:50 +0600

    add solution for finding the minimum in a rotated sorted array

---
+++ src/binarySearch/FindMinimumInRotatedSortedArray/Main.java
@@ -0,0 +1,9 @@
+package binarySearch.FindMinimumInRotatedSortedArray;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {3,4,5,1,2};
+        Solution solution = new Solution();
+        System.out.println(solution.findMin(nums));
+    }
+}
